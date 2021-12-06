19e9514186c3e13fa2641556e44a749b6c02e196
Yuyi <yuyi365@gmail.com>
2021-12-05 20:23:18 -0500

    commit tree 21326977904524c97f101840a13988a8dd6567f2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < Sinatra::Base
-  set default_content_type: "application/json"
-  
+  set default_content_type: 'application/json'
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
   end
-  
+
   get '/bakeries/:id' do
     bakery = Bakery.find(params[:id])
     bakery.to_json(include: :baked_goods)
@@ -23,4 +23,31 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good =
+      BakedGood.create(
+        name: params[:name],
+        price: params[:price],
+        bakery_id: params[:bakery_id],
+      )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(name: params[:name])
+    bakery.to_json
+  end
+
+  delete '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.destroy
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
 end
